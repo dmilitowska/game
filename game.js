7873b362391b7d71315355fc68a8cd25a234022b
dominika militowska <dominika.militowska@gmail.com>
2023-01-20 11:21:30 +0000

    board 2

---
+++ game.js
@@ -0,0 +1,21 @@
+let board = {
+    1: ' ',
+    2: ' ',
+    3: ' ',
+    4: ' ',
+    5: ' ',
+    6: ' ',
+    7: ' ',
+    8: ' ',
+    9: ' '
+  };
+function boardOut() {
+    console.log('\n' +
+      ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
+      ' ---------\n' +
+      ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
+      ' ---------\n' +
+      ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n');
+  }
+
+  boardOut()
